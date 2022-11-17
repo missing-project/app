8a14cc8c76195c7f8d480dc4cc2a4c8db0d6aac0
SonByungjin <sgyos000@gmail.com>
2022-11-17 15:52:27 +0900

    Feature: location to user position

--- lib/screens/landing/wigets/map/map_screen.dart
+++ lib/screens/landing/wigets/map/map_screen.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
@@ -9,10 +11,55 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  // 초기 위치: 서울
   static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+    target: LatLng(
+      37.532600,
+      127.024612,
+    ),
     zoom: 14.4746,
   );
+  final Completer<GoogleMapController> _controller = Completer();
+
+  Location location = Location();
+  late bool _serviceEnabled;
+  late PermissionStatus _permissionGranted;
+
+  void _goToUserLocation(LocationData data) async {
+    CameraPosition userPosition = CameraPosition(
+      target: LatLng(data.latitude!, data.longitude!),
+      zoom: 19.151926040649414,
+    );
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(userPosition));
+  }
+
+  void _locateCheck() async {
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    LocationData userLocation = await location.getLocation();
+    _goToUserLocation(userLocation);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _locateCheck();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +67,9 @@ class _MapScreenState extends State<MapScreen> {
       children: [
         GoogleMap(
           initialCameraPosition: _kGooglePlex,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
         ),
       ],
     );
