0921dc354233776069dad4b63bb38430ad9de1a8
SonByungjin <sgyos000@gmail.com>
2022-11-17 14:59:42 +0900

    Feature: google map android

--- lib/screens/landing/wigets/map/map_screen.dart
+++ lib/screens/landing/wigets/map/map_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
@@ -8,10 +9,19 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('map'),
+    return Stack(
+      children: [
+        GoogleMap(
+          initialCameraPosition: _kGooglePlex,
+        ),
+      ],
     );
   }
 }
