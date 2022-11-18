173328551345bb6da90f0f3c06d32621c18c064c
SonByungjin <sgyos000@gmail.com>
2022-11-18 16:53:40 +0900

    Feature: auth bloc consumer and idCheck apply

--- lib/screens/auth/signup/signup_screen.dart
+++ lib/screens/auth/signup/signup_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:missing_application/blocs/auth/auth_bloc.dart';
+import 'package:missing_application/screens/auth/widgets/auth_bloc_consumer.dart';
 import 'package:missing_application/screens/global/global_appbar.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:missing_application/screens/global/loading_stack.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({Key? key}) : super(key: key);
@@ -20,169 +22,188 @@ class _SignUpScreenState extends State<SignUpScreen> {
   String password = '';
   String passwordCheck = '';
   String email = '';
+  bool idUsable = false;
   bool emailAuthrization = false;
   bool termsAgreement = false;
 
   String? get _errorText {
     if (_pwFocusNode.hasFocus && !regex.hasMatch(password)) {
-      return '대소문자, 숫자, 특수기호를 포함하여 8자 이상이어야 합니다';
+      return AppLocalizations.of(context)!.signup_pw_regex;
     }
 
     return null;
+  }
+
+  void _handleIdCheck() {
+    if (id.isNotEmpty) {
+      BlocProvider.of<AuthBloc>(context).add(IdCheck(id: id));
+    }
+  }
+
+  void _idCheckState(bool value) {
+    setState(() {
+      idUsable = value;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: GlobalAppbar(),
-      body: LoadingStack(
-          isLoading: false,
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    child: ListView(
-                      children: [
-                        SignUpPadding(
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: TextFormFieldStyle(
-                                  obscure: false,
-                                  label: AppLocalizations.of(context)!.id,
-                                  onChanged: ((value) {
-                                    setState(() {
-                                      id = value;
-                                    });
-                                  }),
-                                  validator: (value) {
-                                    return value == null || value.isEmpty
-                                        ? AppLocalizations.of(context)!
-                                            .signup_id_invalid
-                                        : null;
-                                  },
-                                ),
+      body: AuthBlocConsumer(
+        loaded: () {
+          Navigator.pop(context);
+        },
+        idCheck: _idCheckState,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: ListView(
+                    children: [
+                      SignUpPadding(
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: TextFormFieldStyle(
+                                obscure: false,
+                                label: AppLocalizations.of(context)!.id,
+                                onChanged: ((value) {
+                                  setState(() {
+                                    id = value;
+                                  });
+                                }),
+                                validator: (value) {
+                                  return value == null || value.isEmpty
+                                      ? AppLocalizations.of(context)!
+                                          .signup_id_invalid
+                                      : null;
+                                },
                               ),
-                              TextButton(
-                                onPressed: () {},
-                                child: Text('중복 검사'),
-                              ),
-                            ],
-                          ),
+                            ),
+                            TextButton(
+                              onPressed: _handleIdCheck,
+                              child: Text(AppLocalizations.of(context)!
+                                  .signup_id_duplicate_btn),
+                            ),
+                          ],
                         ),
-                        SignUpPadding(
-                          child: TextFormFieldStyle(
-                            obscure: false,
-                            label: AppLocalizations.of(context)!.password,
-                            errorTxt: _errorText,
-                            focusNode: _pwFocusNode,
-                            onChanged: ((value) {
-                              setState(() {
-                                password = value;
-                              });
-                            }),
-                            validator: (value) {
-                              return value == null ||
-                                      value.isEmpty ||
-                                      !regex.hasMatch(value)
-                                  ? AppLocalizations.of(context)!
-                                      .signup_pw_invalid
-                                  : null;
-                            },
-                          ),
+                      ),
+                      SignUpPadding(
+                        child: TextFormFieldStyle(
+                          obscure: false,
+                          label: AppLocalizations.of(context)!.password,
+                          errorTxt: _errorText,
+                          focusNode: _pwFocusNode,
+                          onChanged: ((value) {
+                            setState(() {
+                              password = value;
+                            });
+                          }),
+                          validator: (value) {
+                            return value == null ||
+                                    value.isEmpty ||
+                                    !regex.hasMatch(value)
+                                ? AppLocalizations.of(context)!
+                                    .signup_pw_invalid
+                                : null;
+                          },
                         ),
-                        SignUpPadding(
-                          child: TextFormFieldStyle(
-                            obscure: false,
-                            label: AppLocalizations.of(context)!.passwordCheck,
-                            errorTxt: _pwChFocusNode.hasFocus &&
+                      ),
+                      SignUpPadding(
+                        child: TextFormFieldStyle(
+                          obscure: false,
+                          label: AppLocalizations.of(context)!.passwordCheck,
+                          errorTxt: _pwChFocusNode.hasFocus &&
+                                  password != passwordCheck
+                              ? AppLocalizations.of(context)!
+                                  .signup_pwCh_invalid
+                              : null,
+                          focusNode: _pwChFocusNode,
+                          onChanged: ((value) {
+                            setState(() {
+                              passwordCheck = value;
+                            });
+                          }),
+                          validator: (value) {
+                            return value == null ||
+                                    value.isEmpty ||
                                     password != passwordCheck
                                 ? AppLocalizations.of(context)!
                                     .signup_pwCh_invalid
-                                : null,
-                            focusNode: _pwChFocusNode,
-                            onChanged: ((value) {
+                                : null;
+                          },
+                        ),
+                      ),
+                      SignUpPadding(
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: TextFormFieldStyle(
+                                obscure: false,
+                                label:
+                                    AppLocalizations.of(context)!.signup_email,
+                                onChanged: (value) {
+                                  setState(() {
+                                    email = value;
+                                  });
+                                },
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: () {},
+                              child: Text(AppLocalizations.of(context)!
+                                  .signup_authrization),
+                            )
+                          ],
+                        ),
+                      ),
+                      RichText(
+                        text: TextSpan(
+                          text: AppLocalizations.of(context)!.signup_terms,
+                          style: DefaultTextStyle.of(context).style,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Checkbox(
+                            value: termsAgreement,
+                            onChanged: (value) {
                               setState(() {
-                                passwordCheck = value;
+                                termsAgreement = value!;
                               });
-                            }),
-                            validator: (value) {
-                              return value == null ||
-                                      value.isEmpty ||
-                                      password != passwordCheck
-                                  ? AppLocalizations.of(context)!
-                                      .signup_pwCh_invalid
-                                  : null;
                             },
                           ),
-                        ),
-                        SignUpPadding(
-                          child: Row(
-                            children: [
-                              Expanded(
-                                child: TextFormFieldStyle(
-                                  obscure: false,
-                                  label: '이메일',
-                                  onChanged: (value) {
-                                    setState(() {
-                                      email = value;
-                                    });
-                                  },
-                                ),
-                              ),
-                              TextButton(
-                                onPressed: () {},
-                                child: Text('인증'),
-                              )
-                            ],
-                          ),
-                        ),
-                        RichText(
-                          text: TextSpan(
-                            text: 'Hello',
-                            style: DefaultTextStyle.of(context).style,
-                          ),
-                        ),
-                        Row(
-                          children: [
-                            Checkbox(
-                              value: termsAgreement,
-                              onChanged: (value) {
-                                setState(() {
-                                  termsAgreement = value!;
-                                });
-                              },
-                            ),
-                            Text('약관에 동의 합니다')
-                          ],
-                        )
-                      ],
-                    ),
+                          Text(AppLocalizations.of(context)!.signup_terms_agree)
+                        ],
+                      )
+                    ],
                   ),
                 ),
-                SizedBox(
-                  width: double.infinity,
-                  child: TextButton(
-                    onPressed:
-                        termsAgreement && _formKey.currentState!.validate()
-                            ? () {}
-                            : null,
-                    style: TextButton.styleFrom(
-                      disabledBackgroundColor: Colors.grey[100],
-                      disabledForegroundColor: Colors.black,
-                      backgroundColor: Theme.of(context).primaryColor,
-                      foregroundColor: Colors.white,
-                    ),
-                    child: Text('회원가입'),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: TextButton(
+                  onPressed: termsAgreement && _formKey.currentState!.validate()
+                      ? () {}
+                      : null,
+                  style: TextButton.styleFrom(
+                    disabledBackgroundColor: Colors.grey[100],
+                    disabledForegroundColor: Colors.black,
+                    backgroundColor: Theme.of(context).primaryColor,
+                    foregroundColor: Colors.white,
                   ),
+                  child: Text(AppLocalizations.of(context)!.signup_btn),
                 ),
-              ],
-            ),
-          )),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/screens/auth/widgets/auth_bloc_consumer.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:missing_application/blocs/auth/auth_bloc.dart';
+import 'package:missing_application/screens/global/loading_stack.dart';
+
+class AuthBlocConsumer extends StatefulWidget {
+  const AuthBlocConsumer({
+    super.key,
+    required this.child,
+    required this.loaded,
+    this.idCheck,
+  });
+
+  final Widget child;
+  final Function loaded;
+  final Function(bool)? idCheck;
+
+  @override
+  State<AuthBlocConsumer> createState() => _AuthBlocConsumerState();
+}
+
+class _AuthBlocConsumerState extends State<AuthBlocConsumer> {
+  @override
+  Widget build(BuildContext _) {
+    return BlocConsumer<AuthBloc, AuthState>(
+      listenWhen: (previous, current) => ModalRoute.of(context)!.isCurrent,
+      listener: (_, state) {
+        if (state is AuthError) {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text('${state.error}')));
+        }
+
+        if (state is AuthLoaded) {
+          widget.loaded();
+        }
+
+        if (state is AuthIdCheck && widget.idCheck != null) {
+          widget.idCheck!(state.isUsable);
+        }
+      },
+      builder: (_, state) {
+        return LoadingStack(
+          isLoading: state is AuthLoading,
+          child: widget.child,
+        );
+      },
+    );
+  }
+}
